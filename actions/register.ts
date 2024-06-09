9b42c1cf1755ce92d53ef60ecc64bfb1c70a073b
Shivam <shivamk.services@gmail.com>
2024-06-09 13:47:10 +0530

    register user logic completed

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,32 @@
 "use server";
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid Fields" };
   }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  return { success: "Email Sent!" };
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email Already In Use." };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+  return { success: "User Created!" };
 };
