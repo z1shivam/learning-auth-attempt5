3f4fb1c6d5a058da2fbcc1168186bebab98b7880
Shivam <shivamk.services@gmail.com>
2024-06-09 09:51:24 +0530

    created signin button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/Auth/LoginButton";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export default function Home() {
-  return <main>Shivam can code!</main>;
+  return (
+    <main className="h-full flex justify-center items-center flex-col bg-emerald-900">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐SimpleAuth
+        </h1>
+        <p className="text-white text-lg">Authentication Service with AuthJS</p>
+        <div>
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
